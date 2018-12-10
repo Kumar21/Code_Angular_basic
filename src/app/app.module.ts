868b9d71410ca0a39152dc1976c1baf4e5d22eb2
Kumar21 <perfectks21@gmail.com>
2018-12-10 18:48:57 +0530

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { CourseService } from './course/course.service';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseComponent
+    CourseComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [CourseService],
   bootstrap: [AppComponent]
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,21 +5,54 @@ import { CourseService } from './course.service';
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css'],
-/*   template:`
-        <p>
-        course works!
-        </p>
-        <h3>List of Courses</h3>
-        <ul>
-          <li *ngFor="let course of courses">{{course}}</li>
-        </ul>
-  ` */
 })
 export class CourseComponent implements OnInit {
   courses;
-  constructor(service : CourseService) {
-    this.courses=service.getCourse()
-   }
+  targetvalue="";
+  email1= "Shanu@gmail.com";
+  isActiveClassBinding = true;
+  imageurl = "https://upload.wikimedia.org/wikipedia/en/7/7d/Minions_characters.png";
+  wikkiImage = "https://en.wikipedia.org/static/favicon/wikipedia.ico"
+  courseData={
+    title : "The component Angular courses",
+    rating :4.4546,
+    students : 36548,
+    price : 1355.15,
+    releaseDAte : new Date(2016,3,1)
+  }
+  PipesText ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  onDivClicked() {
+    console.log("div element is clicked");
+  }
+
+  onSave($event) {
+    //to stop event bubblink(calling all the parent attribute event)
+    $event.stopPropagation();
+    console.log("Button event applied", $event);
+  }
+
+  onKeyUpEvent($event) {
+    if ($event.keyCode === 13)
+      console.log("Enter is pressed");
+  }
+  onKeyUp(){
+    console.log("Enter is pressed angular event");
+  }
+
+  onKeyUpEmail(email){
+    console.log("You typed"+email);
+  }
+  onKeyUpEventValue($event){
+    this.targetvalue=$event.target.value;
+    if($event.keyCode===13)
+    console.log("You typed"+$event.target.value);
+  }
+  onKeyUpTowWay(){
+  console.log(this.email1); 
+  }
+  constructor(service: CourseService) {
+    this.courses = service.getCourse()
+  }
   ngOnInit() {
   }
 
