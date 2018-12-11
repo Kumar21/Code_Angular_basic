b7d70b115feb5379cb2c0685ab387e1797bac071
Kumar21 <perfectks21@gmail.com>
2018-12-11 18:36:44 +0530

    new comit

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit ,Input} from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  @Input()isFav:boolean;
+  @Input('is-fav')isFav:boolean;
   constructor() { }
 
   ngOnInit() {
